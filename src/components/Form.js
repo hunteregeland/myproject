1f592bb99334533a2693daa2fd2593589f5997ad
Hunter Egeland <hegeland@my.gcu.edu>
2022-09-26 13:59:09 -0700

    up to lecture 32

---
+++ src/components/Form.js
@@ -0,0 +1,52 @@
+import { Component } from "react";
+
+class Form extends Component {
+  state = {
+    firstname: "",
+    lastname: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      firstname: event.target.value,
+    });
+    console.log(this.state.firstname);
+  };
+
+  handleLastNameChange = (event) => {
+    this.setState({
+      lastname: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault(); // stops form from refreshing the page on submit
+    console.log({
+      fname: this.state.firstname,
+      lname: this.state.lastname,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        Form
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.firstname}
+          ></input>
+
+          <input
+            value={this.state.lastname}
+            type="text"
+            onChange={this.handleLastNameChange}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default Form;

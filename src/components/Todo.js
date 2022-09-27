d600aee56c2f72bd2be22b93050c5b23c3bf5cd9
Hunter Egeland <hegeland@my.gcu.edu>
2022-09-26 23:17:10 -0700

    lecture 33

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,15 +2,25 @@ import { useState } from "react";
 
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
   const handleChange = (event) => {
     setTodo(event.target.value);
-    console.log(todo)
+    //console.log(todo);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let tempList = todoList;
+    tempList.push(todo);
+    setTodoList(tempList);
+    console.log(todoList);
+    setTodo("");
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input value={todo} onChange={handleChange} type="text"></input>
         <button type="submit">Add</button>
       </form>
